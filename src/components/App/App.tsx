fa87463278086ef9e799d9d9155f927d99209342
Nicolas BERNARD <nicolas.bernard@rhino.fi>
2023-06-14 11:32:41 +0200

    weather-app-challenge

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import { WeatherCard } from '../WeatherCard/WeatherCard';
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <WeatherCard />
+      </header>
+    </div>
+  );
+}
---
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -0,0 +1,12 @@
+import './WeatherCard.css'
+
+export const WeatherCard = () => {
+  
+    // TODO: implement the required logic
+
+  return (
+    <div className='WeatherCard'>
+      <p className='city'>London</p>
+    </div>
+  )
+}
